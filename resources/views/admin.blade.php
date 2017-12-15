a917a587757baefb0acb2b83fec845acd6c05739
Cipher66 <schavezfe16dw@ikzubirimanteo.com>
2017-12-15 13:55:23 +0100

    >Formulario de edición por modal pop-up en vez de vista externa.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,11 +4,6 @@
 
     <p><h2><u>Panel de administración de usuarios</u></h2></p>
 
-        <form id="formulario">
-            <h1>hola</h1>
-            <input type="text"/>
-        </form>
-
 <div id="tabla">
 
     <table border="1px solid black">
@@ -24,10 +19,41 @@
             <td>{{ $data->name }}</td>
             <td>{{ $data->email }}</td>
             <td><a href="adminEliminarUsuarios/{{ $data -> id }}" class="miBoton">Eliminar</a></td>
-            <td><base href="/adminEditarUsuario/{{ $data -> id }}" class="botonEditar" />Editar</td>
+            <td><button type="button"
+                    class="botonEditar"
+                    data-toggle="modal"
+                    data-target="#myModal">Editar</button></td>
             <td></td>
         </tr>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+                <!-- Contenido del modal -->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button"
+                                class="close"
+                                data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Editar Usuario</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Some text in the modal</p>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button"
+                                class="botonCerrar"
+                                data-dismiss="modal">Cerrar</button>
+                        <button type="button"
+                                class="botonActualizar">Actualizar</button>
+                    </div>
+                </div>
+                <!-- /Contenido del modal -->
+            </div>
+        </div>
+        <!-- /Modal -->
         @endforeach
+
     </table>
 
 </div>
@@ -36,4 +62,25 @@
 
 <!--
 action="adminActualizarUsuario"
+<table>
+<tr>
+<input type="hidden"value="{{ $data->id }}" />
+</tr>
+<tr>
+<td>
+Nombre:
+</td>
+<td>
+<input type="text" value="{{ $data->name }}" />
+</td>
+</tr>
+<tr>
+<td>
+Email:
+</td>
+<td>
+<input type="text" value="{{ $data->email }}" />
+</td>
+</tr>
+</table>
 -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
 
 
     <!-- Latest compiled and minified CSS -->
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
 
 
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- BootBox -->
@@ -29,7 +29,7 @@
     <!-- Google Fonts (Montserrat)-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <!-- /Google Fonts (Montserrat)-->
-    <link href="css/estiloshome.css" rel="stylesheet">
+    <link href="css/estiloshome.css" rel="stylesheet" />
 
 
 
